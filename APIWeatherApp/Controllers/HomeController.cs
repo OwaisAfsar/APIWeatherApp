34375acc7d48157b46f8a27b3644b4124279ca9e
owais <owais@SHADOWMSI>
2023-02-09 18:50:32 +0100

    Add project files.

---
+++ APIWeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using APIWeatherApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using Nancy.Json;
+
+namespace APIWeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        //string baseURL = "https://api.openweathermap.org/data/2.5/weather?q=Munich&APPID=3b89076394786db067776f1f6da11b1f";
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+        public async Task<ActionResult> Index(string cityName)
+        {
+            string baseURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&APPID=3b89076394786db067776f1f6da11b1f";
+            
+            ResultModel resultModel = new ResultModel();
+
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri(baseURL);
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage responseMessage = await httpClient.GetAsync(baseURL);
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var results = responseMessage.Content.ReadAsStringAsync().Result;
+                    RootObject weatherInformation = (new JavaScriptSerializer()).Deserialize<RootObject>(results);
+
+                    resultModel = WeatherData.GetWeatherData(resultModel, weatherInformation);
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                }
+            }
+            return View("Index", resultModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ APIWeatherApp/Models/WeatherData.cs
@@ -0,0 +1,28 @@
+﻿namespace APIWeatherApp.Models
+{
+    public class WeatherData
+    {
+        public static ResultModel GetWeatherData(ResultModel _resultModel, RootObject _weatherInformation)
+        {
+            _resultModel.City = _weatherInformation.name;
+            _resultModel.Country = _weatherInformation.sys.country;
+            _resultModel.description = _weatherInformation.weather[0].main;
+            _resultModel.Temperature = string.Format("{0} °C", ValueConverter(_weatherInformation.main.temp));
+            _resultModel.TemperatureMinimum = string.Format("{0} °C", ValueConverter(_weatherInformation.main.temp_min));
+            _resultModel.TemperatureMaximum = string.Format("{0} °C", ValueConverter(_weatherInformation.main.temp_max));
+            _resultModel.AirPressure = string.Format("{0} hPa", _weatherInformation.main.pressure);
+            _resultModel.Humidity = string.Format("{0}%", _weatherInformation.main.humidity);
+            _resultModel.WindSpeed = string.Format("{0} m/s", _weatherInformation.wind.speed);
+            _resultModel.WindDirection = string.Format("{0}°", _weatherInformation.wind.deg);
+            _resultModel.CloudCover = _weatherInformation.clouds.all.ToString();
+
+            return _resultModel;
+        }
+
+        public static double ValueConverter(double _value)
+        {
+            double convertedValue = _value - 273.15;
+            return Math.Round(convertedValue, 2);
+        }
+    }
+}
